f4a449782ec1b61c083ec33310ff04deb9fd95f6
verhl06 <128389974+verhl06@users.noreply.github.com>
2024-05-22 14:12:16 +0200

    Fix url routing

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +8,11 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(private router: Router) {
+  }
+
   handleSearch(searchInput: string): void {
     const searchTerm = searchInput.toLowerCase();
-    const url = new URL("/table", window.location.href);
-    url.searchParams.set("search", searchTerm);
-    window.location.href = url.href;
+    this.router.navigate(['/table'], { queryParams: { search: searchTerm } });
   }
 }
